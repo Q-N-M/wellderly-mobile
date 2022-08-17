983b152f85e6c13f21a678aa3fe01ae27f967130
hanifahaputri <hanifahaputri@gmail.com>
2022-08-17 14:06:37 +1000

    feat: onboarding component

--- src/screens/TabOneScreen.tsx
+++ src/screens/TabOneScreen.tsx
@@ -1,17 +1,24 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '../components/EditScreenInfo';
+import { ScrollView, StyleSheet } from 'react-native';
+import OnBoarding from '../components/OnBoarding';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../../types';
 
+const coffee = require('../assets/images/coffee.png');
+
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-    </View>
-  );
+    return (
+        <ScrollView>
+        <View style={styles.container}>
+      {/*<Text style={styles.title}>Tab One</Text>Text*/}
+      {/*<View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />*/}
+      {/*<EditScreenInfo path="/screens/TabOneScreen.tsx" />*/}
+        <OnBoarding
+          title={"hehe"}
+          subtitle={"hehe"}
+          img={coffee}/>
+        </View>
+</ScrollView>
+);
 }
 
 const styles = StyleSheet.create({
