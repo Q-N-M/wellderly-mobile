c7c3f3014a64cdbdb3abb5882f5c66409f826175
hanifahaputri <hanifahaputri@gmail.com>
2022-08-24 14:18:11 +1000

    feat: added home

--- src/screens/TabOneScreen.tsx
+++ src/screens/TabOneScreen.tsx
@@ -1,39 +1,34 @@
-import { ScrollView, StyleSheet } from 'react-native';
-import OnBoarding from '../components/OnBoarding';
+import { StyleSheet } from 'react-native';
+
+import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../../types';
-
-const coffee = require('../assets/images/coffee.png');
+import Home from '../components/Home';
 
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
     return (
-        <ScrollView>
         <View style={styles.container}>
-      {/*<Text style={styles.title}>Tab One</Text>Text*/}
-      {/*<View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />*/}
-      {/*<EditScreenInfo path="/screens/TabOneScreen.tsx" />*/}
-        <OnBoarding
-          title={"hehe"}
-          subtitle={"hehe"}
-          img={coffee}/>
+            <Text style={styles.title}>Tab One</Text>
+            <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+            <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+            <Home/>
         </View>
-</ScrollView>
-);
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+    separator: {
+        marginVertical: 30,
+        height: 1,
+        width: '80%',
+    },
 });
